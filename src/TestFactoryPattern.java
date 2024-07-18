8b6e3d0b13f268c22d42bfbab403575cd03ada02
s4br0 <aybgni@gmail.com>
2024-07-18 05:31:55 +0200

    third commit and push

--- src/TestFactoryPattern.java
+++ src/TestFactoryPattern.java
@@ -1,5 +1,25 @@
 public class TestFactoryPattern {
     public static void main(String[] args) {
+        // Create an order
+        Order order = new Order();
 
+        // Create observers
+        PriceObserver priceObserver = new PriceObserver();
+        QuantityObserver quantityObserver = new QuantityObserver();
+
+        // Attach observers
+        order.attach(priceObserver);
+        order.attach(quantityObserver);
+
+        // Add items and notify observers
+        System.out.println("First addition:");
+        order.addItem(100.0);
+        order.addItem(150.0);
+
+        // Detach QuantityObserver and make another change
+        order.detach(quantityObserver);
+
+        System.out.println("Second addition (after detaching QuantityObserver):");
+        order.addItem(50.0);
     }
 }
